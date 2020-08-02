9e04171e37bcb309a32e7899e8c0c76a5f479e95
Tomas Milar <tomasmilar4@gmail.com>
2020-08-01 22:13:14 -0300

    test: move PluggyClient init logic to parent class BaseApiIntegrationTest.

---
+++ src/test/java/ai/pluggy/client/integration/BaseApiIntegrationTest.java
@@ -0,0 +1,16 @@
+package ai.pluggy.client.integration;
+
+import ai.pluggy.client.PluggyClient;
+import org.junit.jupiter.api.BeforeEach;
+
+class BaseApiIntegrationTest {
+
+  static final String CLIENT_ID = "906a15c0-fdde-4dc5-9a23-df44455e1fb4";
+  static final String CLIENT_SECRET = "6fc93aec-9166-417c-8363-669167a39ce4";
+  PluggyClient client;
+
+  @BeforeEach
+  void setUp() {
+    client = PluggyClient.builder().clientIdAndSecret(CLIENT_ID, CLIENT_SECRET).build();
+  }
+}
