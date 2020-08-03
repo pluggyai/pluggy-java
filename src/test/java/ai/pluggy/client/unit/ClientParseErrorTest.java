4f37efb8a99d5d503679c1e780bc4962eeffc778
Tomas Milar <tomasmilar4@gmail.com>
2020-08-02 21:33:55 -0300

    feat(client): parse ErrorResponse + update README examples.

---
+++ src/test/java/ai/pluggy/client/unit/ClientParseErrorTest.java
@@ -0,0 +1,55 @@
+package ai.pluggy.client.unit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import ai.pluggy.client.PluggyClient;
+import ai.pluggy.client.response.ErrorResponse;
+import com.google.gson.JsonParseException;
+import java.io.IOException;
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+
+public class ClientParseErrorTest {
+
+  private PluggyClient client;
+
+  @BeforeEach
+  void setUp() {
+    client = PluggyClient.builder()
+      .clientIdAndSecret("client-id", "client-secret")
+      .build();
+  }
+
+  @Test
+  void clientParseError_jsonErrorResponse_ok() throws IOException {
+    // simulate error response
+    String errorMessage = "An update is already in progress, wait until the last execution ends";
+    Integer errorCode = 400;
+    String errorJson = "{\"message\":\"" + errorMessage + "\",\"code\":" + errorCode + "}";
+    Response<Object> errorResponse = Response
+      .error(400, ResponseBody.create(errorJson, MediaType.parse("application/json")));
+
+    // expect parsed error response
+    ErrorResponse errorResponseParsed = client.parseError(errorResponse);
+
+    assertNotNull(errorResponseParsed);
+    assertEquals(errorMessage, errorResponseParsed.getMessage());
+    assertEquals(errorCode, errorResponseParsed.getCode());
+  }
+
+  @Test
+  void clientParseError_malformedErrorResponse_fails() throws IOException {
+    // simulate error response
+    String malformedErrorJson = "{";
+    Response<Object> errorResponse = Response
+      .error(400, ResponseBody.create(malformedErrorJson, MediaType.parse("application/json")));
+
+    // expect parsed error response
+    assertThrows(JsonParseException.class, () -> client.parseError(errorResponse));
+  }
+}
