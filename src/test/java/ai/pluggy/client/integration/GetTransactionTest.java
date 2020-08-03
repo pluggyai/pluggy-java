e993440a68651a06ce9cd3f9d0d552c33d2042df
Tomas Milar <tomasmilar4@gmail.com>
2020-08-03 02:51:20 -0300

    feat: implement PluggyApiService.getTransaction(String transactionId) method.

---
+++ src/test/java/ai/pluggy/client/integration/GetTransactionTest.java
@@ -0,0 +1,32 @@
+package ai.pluggy.client.integration;
+
+import static ai.pluggy.client.integration.helper.TransactionHelper.getFirstAccountTransactions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import ai.pluggy.client.response.Transaction;
+import ai.pluggy.client.response.TransactionsResponse;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+
+public class GetTransactionTest extends BaseApiIntegrationTest {
+
+  @SneakyThrows
+  @Test
+  void getTransaction_byExistingId_ok() {
+    TransactionsResponse firstAccountTransactions = getFirstAccountTransactions(client);
+    String existingTransactionId = firstAccountTransactions.getResults().get(0).getId();
+
+    // request existing transaction
+    Response<Transaction> transactionResponse = client.service()
+      .getTransaction(existingTransactionId)
+      .execute();
+    
+    Transaction transaction = transactionResponse.body();
+
+    // expect retrieved transaction to be not null, and id to match the requested id
+    assertNotNull(transaction);
+    assertEquals(transaction.getId(), existingTransactionId);
+  }
+}
---
+++ src/test/java/ai/pluggy/client/integration/helper/TransactionHelper.java
@@ -0,0 +1,39 @@
+package ai.pluggy.client.integration.helper;
+
+import static ai.pluggy.client.integration.helper.AccountHelper.retrieveFirstAccountId;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ai.pluggy.client.PluggyClient;
+import ai.pluggy.client.response.TransactionsResponse;
+import java.io.IOException;
+import lombok.extern.log4j.Log4j2;
+import retrofit2.Response;
+
+@Log4j2
+public class TransactionHelper {
+
+  public static TransactionsResponse getFirstAccountTransactions(PluggyClient client)
+    throws IOException, InterruptedException {
+    // precondition: retrieve accounts data
+    String firstAccountId = retrieveFirstAccountId(client);
+
+    log.info("Retrieving transactions by account id '" + firstAccountId + "' (the first one)");
+
+    // get first account transactions
+    Response<TransactionsResponse> transactionsResponse = client.service()
+      .getTransactions(firstAccountId)
+      .execute();
+
+    TransactionsResponse transactions = transactionsResponse.body();
+
+    // assert transactions result not null and not empty
+    assertNotNull(transactions);
+    int transactionsCount = transactions.getResults().size();
+    assertTrue(transactionsCount > 0);
+
+    log
+      .info("Got " + transactionsCount + " transactions for account id '" + firstAccountId + "'. ");
+    return transactions;
+  }
+}
