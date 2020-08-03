6bc95e046ccb980e8ac529ba1cd394ef30208203
Tomas Milar <tomasmilar4@gmail.com>
2020-08-03 03:11:25 -0300

    feat: implement PluggyApiService.getInvestments(String accountId) method.

---
+++ src/test/java/ai/pluggy/client/integration/GetInvestmentsTest.java
@@ -0,0 +1,40 @@
+package ai.pluggy.client.integration;
+
+import static ai.pluggy.client.integration.helper.ItemHelper.createPluggyBankItem;
+import static ai.pluggy.client.integration.helper.ItemHelper.getItemStatus;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ai.pluggy.client.integration.util.Poller;
+import ai.pluggy.client.response.InvestmentsResponse;
+import ai.pluggy.client.response.ItemResponse;
+import java.util.Objects;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+
+public class GetInvestmentsTest extends BaseApiIntegrationTest {
+
+  @SneakyThrows
+  @Test
+  void getInvestments_afterExecutionCompleted_responseWithResults() {
+    // precondition: start pluggy bank execution & wait for completion
+    ItemResponse pluggyBankExecution = createPluggyBankItem(client);
+
+    // poll check of connector item status until it's completed (status: "UPDATED")
+    Poller.pollRequestUntil(
+      () -> getItemStatus(client, pluggyBankExecution.getId()),
+      (ItemResponse itemResponse) -> Objects.equals(itemResponse.getStatus(), "UPDATED"),
+      500, 30000
+    );
+
+    // get investments request
+    Response<InvestmentsResponse> investmentsResponse = client.service().getInvestments(pluggyBankExecution.getId()).execute();
+
+    // expect investments response to be valid & contain 1 or more results
+    assertTrue(investmentsResponse.isSuccessful());
+    InvestmentsResponse investments = investmentsResponse.body();
+    assertNotNull(investments);
+    assertTrue(investments.getResults().size() > 0);
+  }
+}
