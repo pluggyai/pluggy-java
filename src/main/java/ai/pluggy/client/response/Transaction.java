aaec596eb56549910d976df16a983af4dae9b4ae
Tomas Milar <tomasmilar4@gmail.com>
2020-08-03 02:08:36 -0300

    feat: implement PluggyApiService.getTransactions(String accountId) method

--- src/main/java/ai/pluggy/client/response/Transaction.java
+++ src/main/java/ai/pluggy/client/response/Transaction.java
@@ -1,5 +1,15 @@
 package ai.pluggy.client.response;
 
+import lombok.Data;
+
+@Data
 public class Transaction {
 
+  String id;
+  String description;
+  String currencyCode;
+  Double amount;
+  String date;
+  Double balance;
+  String category;
 }
