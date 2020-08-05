86800a7fe35c38985b3b2520ff836593614b6aa1
Tomas Milar <tomasmilar4@gmail.com>
2020-08-05 20:27:27 -0300

    fix(test): refactor 'updateItem_afterWaitingForCreation_ok' test to wait for status & use pluggy bank.

--- src/test/java/ai/pluggy/client/integration/UpdateItemTest.java
+++ src/test/java/ai/pluggy/client/integration/UpdateItemTest.java
@@ -1,6 +1,8 @@
 package ai.pluggy.client.integration;
 
 import static ai.pluggy.client.integration.helper.ItemHelper.createItem;
+import static ai.pluggy.client.integration.helper.ItemHelper.createPluggyBankItem;
+import static ai.pluggy.client.integration.helper.ItemHelper.getItemStatus;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -8,10 +10,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import ai.pluggy.client.integration.util.Poller;
 import ai.pluggy.client.request.ParametersMap;
 import ai.pluggy.client.request.UpdateItemRequest;
 import ai.pluggy.client.response.ErrorResponse;
 import ai.pluggy.client.response.ItemResponse;
+import java.util.Objects;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import retrofit2.Response;
@@ -53,8 +57,7 @@ public class UpdateItemTest extends BaseApiIntegrationTest {
   @Test
   void updateItem_afterWaitingForCreation_ok() {
     // precondition: an item already exists
-    Integer connectorId = 1;
-    ItemResponse createdItemResponse = createItem(client, connectorId);
+    ItemResponse createdItemResponse = createPluggyBankItem(client);
 
     // build update item request
     String newWebhookUrl = "localhost:3000";
@@ -62,9 +65,13 @@ public class UpdateItemTest extends BaseApiIntegrationTest {
       .webhookUrl(newWebhookUrl)
       .build();
 
-    // wait for creation finish before updating, to prevent update request error 400. 
-    // TODO use polling to ensure createdItem status becomes "UPDATED"
-    Thread.sleep(30000);
+    // wait for creation finish (status: "UPDATED") before updating, to prevent update request error 400.
+    Poller.pollRequestUntil(
+      () -> getItemStatus(client, createdItemResponse.getId()),
+      (ItemResponse itemStatusResponse) -> Objects
+        .equals(itemStatusResponse.getStatus(), "UPDATED"),
+      500, 45000
+    );
 
     // run update item request
     Response<ItemResponse> updateItemResponse = client.service()
