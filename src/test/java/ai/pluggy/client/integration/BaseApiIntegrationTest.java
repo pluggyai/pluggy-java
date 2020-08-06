599e7623a8bf916b6aeb5e4b02098592a8ebc393
Tomas Milar <tomasmilar4@gmail.com>
2020-08-05 21:11:48 -0300

    feat(test): use env vars for client secrets & baseUrl in integration tests.

--- src/test/java/ai/pluggy/client/integration/BaseApiIntegrationTest.java
+++ src/test/java/ai/pluggy/client/integration/BaseApiIntegrationTest.java
@@ -1,20 +1,45 @@
 package ai.pluggy.client.integration;
 
 import ai.pluggy.client.PluggyClient;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.platform.commons.util.StringUtils;
 
 class BaseApiIntegrationTest {
 
+  static final String CLIENT_ID = System.getenv("PLUGGY_CLIENT_ID");
+  private static final String CLIENT_SECRET = System.getenv("PLUGGY_CLIENT_SECRET");
   static final String TEST_BASE_URL = System.getenv("PLUGGY_BASE_URL");
-  static final String CLIENT_ID = "906a15c0-fdde-4dc5-9a23-df44455e1fb4";
-  static final String CLIENT_SECRET = "6fc93aec-9166-417c-8363-669167a39ce4";
+
   PluggyClient client;
 
   @BeforeEach
   void setUp() {
+    checkEnvErrors();
     client = PluggyClient.builder()
       .baseUrl(TEST_BASE_URL)
       .clientIdAndSecret(CLIENT_ID, CLIENT_SECRET)
       .build();
+  }
+
+  private void checkEnvErrors() {
+    List<String> missingEnvVars = new ArrayList<>();
+    if (StringUtils.isBlank(CLIENT_ID)) {
+      missingEnvVars.add("PLUGGY_CLIENT_ID");
+    }
+    if (StringUtils.isBlank(CLIENT_SECRET)) {
+      missingEnvVars.add("PLUGGY_CLIENT_SECRET");
+    }
+    if (StringUtils.isBlank(TEST_BASE_URL)) {
+      missingEnvVars.add("PLUGGY_BASE_URL");
+    }
+    if (missingEnvVars.size() > 0) {
+      String envVarsListString = missingEnvVars.stream()
+        .map(varName -> "'" + varName + "'")
+        .collect(Collectors.joining(", "));
+      throw new IllegalStateException("Must define " + envVarsListString + " env var(s)!");
+    }
   }
 }
