14324a575898d16cafad1f69a4426ba4cdd4fd94
Tomas Milar <tomasmilar4@gmail.com>
2020-08-18 17:00:14 -0300

    feat(api): implement no-params updateItem(id) method overload.

--- src/main/java/ai/pluggy/client/request/UpdateItemRequest.java
+++ src/main/java/ai/pluggy/client/request/UpdateItemRequest.java
@@ -3,10 +3,12 @@ package ai.pluggy.client.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateItemRequest {
   Integer connectorId;
   ParametersMap parameters;
