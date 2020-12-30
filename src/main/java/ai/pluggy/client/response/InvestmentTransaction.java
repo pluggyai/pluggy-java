c6b46050601bab803a94c406e55569c54b842d85
Tomas Milar <tomasmilar4@gmail.com>
2020-12-30 03:43:19 -0300

    feat(investment): map Investment missing fields & InvestmentTransaction[] transactions.
        New mapped subtypes:
        * InvestmentStatus
        * InvestmentType
        * InvestmentTransaction
        * InvestmentTransactionType

---
+++ src/main/java/ai/pluggy/client/response/InvestmentTransaction.java
@@ -0,0 +1,15 @@
+package ai.pluggy.client.response;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class InvestmentTransaction {
+
+  InvestmentTransactionType type;
+  Double quantity;
+  Double value;
+  Double amount;
+  Date date;
+  Date tradeDate;
+}
