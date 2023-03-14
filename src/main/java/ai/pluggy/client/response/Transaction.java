c93c92735cb2def686679b5a27c96a517a4789d4
NicolasMontone <montonenicolas01@gmail.com>
2023-03-14 10:02:42 -0300

    feat(Transaction): add creditCardMetadata and descriptionRaw fields in model and make date Date type.

--- src/main/java/ai/pluggy/client/response/Transaction.java
+++ src/main/java/ai/pluggy/client/response/Transaction.java
@@ -1,19 +1,23 @@
 package ai.pluggy.client.response;
+import java.util.Date;
+
 
 import lombok.Data;
 
 @Data
 public class Transaction {
-
   String id;
+  String accountId;
   String description;
+  String descriptionRaw;
   String currencyCode;
   Double amount;
-  String date;
+  Date date;
   Double balance;
   String category;
   String providerCode;
   TransactionStatus status;
   PaymentData paymentData;
+  TransactionCreditCardMetadata creditCardMetadata;
   TransactionType type;
 }
