dbfd7aa4eaf7ffce9c841287f8febac53f54bf91
Francisco Disalvo <francisco.disalvo90@gmail.com>
2024-07-17 14:21:46 -0300

    feat: update pom

--- src/test/java/ai/pluggy/client/unit/WebhookEventParseTest.java
+++ src/test/java/ai/pluggy/client/unit/WebhookEventParseTest.java
@@ -15,14 +15,14 @@ public class WebhookEventParseTest {
   @Test
   void webhookEventParse_jsonPayload_ok() throws IOException {
     String webhookNotificationJson = "{\n" +
-      "  \"event\":\"item/created\",\n" +
-      "  \"id\":\"a5c763cb-0952-457b-9936-630f79c5b016\",\n" +
-      "  \"itemId\":\"a5c763cb-0952-457b-9936-630f79c5b016\",\n" +
-      "  \"triggeredBy\": \"USER\"\n" +
-      "}";
+        "  \"event\":\"item/created\",\n" +
+        "  \"id\":\"a5c763cb-0952-457b-9936-630f79c5b016\",\n" +
+        "  \"itemId\":\"a5c763cb-0952-457b-9936-630f79c5b016\",\n" +
+        "  \"triggeredBy\": \"USER\"\n" +
+        "}";
 
     WebhookEventPayload webhookPayload = Utils.parseWebhookEventPayload(webhookNotificationJson);
-    
+
     assertEquals(WebhookEventType.ITEM_CREATED, webhookPayload.getEvent());
     assertEquals("a5c763cb-0952-457b-9936-630f79c5b016", webhookPayload.getId());
     assertEquals("a5c763cb-0952-457b-9936-630f79c5b016", webhookPayload.getItemId());
@@ -32,12 +32,12 @@ public class WebhookEventParseTest {
   @Test
   void webhookEventConnectorStatusUpdatedParse_jsonPayload_ok() throws IOException {
     String webhookNotificationJson = "{\n" +
-    "  \"event\":\"connector/status_updated\",\n" +
-    "  \"data\": {\n\"status\":\"UNSTABLE\"\n},\n" +
-    "  \"id\":\"201\"\n" +
-    "}";
+        "  \"event\":\"connector/status_updated\",\n" +
+        "  \"data\": {\n\"status\":\"UNSTABLE\"\n},\n" +
+        "  \"id\":\"201\"\n" +
+        "}";
     WebhookEventPayload webhookPayload = Utils.parseWebhookEventPayload(webhookNotificationJson);
-    
+
     assertEquals(WebhookEventType.CONNECTOR_STATUS_UPDATED, webhookPayload.getEvent());
     assertEquals("201", webhookPayload.getId());
     assertEquals("UNSTABLE", webhookPayload.getData().getStatus());
